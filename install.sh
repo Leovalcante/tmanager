5e195afab685d475aecb22136e6c37fcc00fcc8d
Valerio Preti <valerio.preti@gmail.com>
2019-10-16 17:44:09 +0200

    Remove useless pip install

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 command -v python3 >/dev/null 2>&1 || { echo >&2 "python3 required but it's not installed. Aborting."; exit 1; }
-pip3 install -r requirements.txt
 python3 setup.py install
